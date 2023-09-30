562f8bfbe1c32ef44b8e8d4637eb2e42ec1b0275
luciana <142445433+lyayam@users.noreply.github.com>
2023-09-29 23:16:38 -0500

    Add files via upload

---
+++ Perro.java
@@ -0,0 +1,19 @@
+/*
+ */
+package animales;
+
+/**
+ *
+ */
+public class Perro extends Animal {
+
+    public Perro(String nombre, String raza, String color, int edad) {
+        super(nombre, raza, color, edad);
+    }
+
+    public Perro() {
+
+        super();
+    }
+
+}
